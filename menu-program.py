a6ed87846503c625410d8c2cf86348c700e1d722
Nitesh <05niteshraj@gmail.com>
2023-07-31 05:55:15 +0530

    Added os module and added more options;

--- menu-program.py
+++ menu-program.py
@@ -1,26 +1,44 @@
+import os
+
+from option_functions import *
+
 isOn = True
 title = "Welcome to the Menu Program!"
 menu = []
 
+
 def setup():
-    global isOn 
+    global isOn
     isOn = True
-    global title 
+    global title
     title = "Welcome to the Menu Program!"
-    global menu 
-    menu = ["1. Option 1", "2. Option 2", "3. Option 3", "4. Exit"]
+    global menu
+    menu = [
+        "1. View Docker Info",
+        "2. List Docker Containers",
+        "3. Run Ansible Playbook",
+        "4. View Disk Space",
+        "5. List Files in Directory",
+        "6. Tell Me a Fortune",
+        "7. Display a Funny Cow",
+        "10. Exit"
+    ]
+
 
 def clearScreen():
-    print("\033c", end="")
+    os.system("clear")
+
 
 def printTitle():
     global title
 
     print("+" + 32 * "-" + "+")
     print("|" + 32 * " " + "|")
-    print("|" + ((33 - len(title)) // 2) * " " + title + ((33 - len(title)) // 2) * " " + "|")
+    print("|" + ((33 - len(title)) // 2) * " " +
+          title + ((33 - len(title)) // 2) * " " + "|")
     print("|" + 32 * " " + "|")
     print("+" + 32 * "-" + "+")
+
 
 def printMenu():
     global title
@@ -31,20 +49,31 @@ def printMenu():
     for i in menu:
         print(i)
 
+
 def menuChoice():
     global isOn
- 
+
     choice = input("Enter your choice: ")
     if choice == "1":
-        print("You chose option 1")
+        view_docker_info()
     elif choice == "2":
-        print("You chose option 2")
+        list_docker_containers()
     elif choice == "3":
-        print("You chose option 3")
-    elif choice == "4" or "q" or "Q":
+        run_ansible_playbook()
+    elif choice == "4":
+        view_disk_space()
+    elif choice == "5":
+        list_files_in_directory()
+    elif choice == "6":
+        tell_me_fortune()
+    elif choice == "7":
+        display_funny_cow()
+    elif choice == "10" or choice.lower() in ["q", "quit", "exit"]:
+        print("Exiting...")
         isOn = False
     else:
         print("Invalid choice")
+
 
 def main():
     global isOn
@@ -59,4 +88,6 @@ def main():
     except KeyboardInterrupt:
         print("Exiting...")
         isOn = False
+
+
 main()
